7695b7a0a9d1b9fc3e2f36ebfc3b81c4e4525d12
Chakir Ardenne <ardenne.chakir@gmail.com>
2021-01-18 14:04:03 +0100

    feat: showing match algo

--- app/src/main/java/com/affinitynow/MatchActivity.java
+++ app/src/main/java/com/affinitynow/MatchActivity.java
@@ -6,7 +6,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.affinitynow.matching.Matching;
+import com.affinitynow.user.Topic;
+import com.affinitynow.user.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MatchActivity extends AppCompatActivity {
+    private List<User> users = new ArrayList<>();
+    
     public static void start(Context context) {
         Intent starter = new Intent(context, MatchActivity.class);
         context.startActivity(starter);
@@ -16,5 +27,51 @@ public class MatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
+        User u1 = new User("Toto");
+        User u2 = new User("Chakir");
+        User u3 = new User("Avi");
+        User u4 = new User("Jean-Michel");
+
+        users.add(u2);
+        users.add(u3);
+        users.add(u4);
+
+        Topic t1 = new Topic("Tennis");
+        Topic t2 = new Topic("FOOT");
+        Topic t3 = new Topic("IT");
+        Topic t4 = new Topic("Cuisine");
+
+        Map<String, Topic> topics1 = new HashMap<>();
+        Map<String, Topic> topics2 = new HashMap<>();
+        Map<String, Topic> topics3 = new HashMap<>();
+        Map<String, Topic> topics4 = new HashMap<>();
+
+        topics1.put("Tennis", t1);
+        topics1.put("FOOT", t2);
+        topics1.put("IT", t3);
+        topics1.put("Cuisine", t4);
+
+        topics2.put("Tennis", t1);
+        topics2.put("FOOT", t2);
+        topics2.put("Cuisine", t4);
+
+        topics3.put("Tennis", t1);
+        topics3.put("Cuisine", t4);
+
+        topics4.put("IT", t3);
+        topics4.put("Cuisine", t4);
+
+        u1.setTopics(topics1);
+        u2.setTopics(topics2);
+        u3.setTopics(topics3);
+        u4.setTopics(topics4);
+
+        Matching matching = new Matching();
+
+        for (User user: users) {
+            if(matching.match(u1, user))
+                System.out.println(String.format("%s match with %s ", u1.getUserName(), user.getUserName()));
+        }
     }
 }
